d915c0e68d202fe3c58b3921d8a2cde151afb181
prajothan <prajothan008@gmail.com>
2025-08-19 17:43:20 +0530

    Update TestrunnerApplication.java

--- src/main/java/com/example/testrunner/TestrunnerApplication.java
+++ src/main/java/com/example/testrunner/TestrunnerApplication.java
@@ -15,7 +15,7 @@ public class TestrunnerApplication {
 	}
 
 	@GetMapping("/testRunner")
-    public String hello(@RequestParam(value = "name", defaultValue = "Test Prajoth AN - Demo 18th Aug") String name) {
+    public String hello(@RequestParam(value = "name", defaultValue = "Test Prajoth AN - Demo Development") String name) {
       return String.format("Hello %s!", name);
     }
 }
